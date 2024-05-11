f0a7994c07c92325d5f04cd03a97cb5876a15baa
茫茫人海一粒沙 <593954214@qq.com>
2024-05-11 22:30:14 +0800

    Update llama3_server.py

--- service/llama3_server.py
+++ service/llama3_server.py
@@ -1,5 +1,8 @@
 from service.LLM_server import *
 import requests
+import util.logging_utils as logging_utils
+
+logger = logging_utils.setup_logger('llama3_server')
 
 import certifi
 print(certifi.where())
@@ -9,30 +12,45 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+requests.packages.urllib3.disable_warnings()
+
+import time
 class Llama3Server(LLM):
     def __init__(self):
         super().__init__("Llama3")
 
-    def completion(self, prompt, history=""):
-        resp = requests.post(
-            url="http://127.0.0.1:8080/completion",
-            # url="http://127.0.0.1:8080/chat/completion",
-            # url="https://lgtcmtaspn1d.nam.nsroot.net:8088/completion",
+    def completion(self, prompt, temperature=0, top_p=0.5, top_k=5, n_predict=600, presence_penalty=0, frequency_penalty=0, history=""):
+        start = time.time()
+        session = requests.session()
+        # fix 407 error while connecting llama3
+        session.trust_env = False
+
+        resp = session.post(
+            url="http://127.0.0.1:9999/completion",
             json={"prompt": prompt,
-                  "temperature": 0.7,
-                  "top_p": 0.5,
-                  "top_k": 40,
-                  "n_predict": 400
+                  "temperature": float(temperature),
+                  "top_p": float(top_p),
+                  "top_k": int(top_k),
+                  "n_predict": int(n_predict),
+                   "presence_penalty": float(presence_penalty),
+                  "frequency_penalty": float(frequency_penalty),
+                  "history": history
                   },
-            # json={"prompt": prompt, "history": history},
-            # data = '{"prompt": "Building a website can be done in 10 simple steps:","n_predict": 128}',
             headers={"Content-Type": "application/json;charset=utf-8"},
-            # verify=False
+            verify=False
 
         )
-        print(resp.json())
-        # print(resp.json()["content"])
-        return resp.json()["content"]
+        print(resp)
+        content = resp.json()["content"]
+        if content:
+            total_time = time.time() - start
+            token_size = len(content)
+            print(f'Total time: {total_time}, Token size: {token_size} Time per token : {total_time / token_size}')
+        print(content)
+
+        return content
 
 if __name__ == '__main__':
     # prompt = "please translate the following text into English, no notes and no explanation. text:'你很坏', result: "

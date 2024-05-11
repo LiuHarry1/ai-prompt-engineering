2e3a2eff230dd0dac08f8720251bd747f6699c41
茫茫人海一粒沙 <593954214@qq.com>
2024-05-11 22:28:21 +0800

    Update llama_completion.py

--- ui_service/llama_completion.py
+++ ui_service/llama_completion.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify, request, jsonify, send_file
 import os
 from service import *
+import util.logging_utils as logging_utils
+
+logger = logging_utils.setup_logger('llama_completion')
 
 llama_completion = Blueprint("llama_completion", __name__)
-
 
 
 @llama_completion.route('/llama/completion', methods=['POST'])
@@ -17,11 +19,26 @@ def completion():
 
     prompt = data.get('prompt')
     llm_name = data.get('llm_name')
-    print('llm name', llm_name)
+    temperature = data.get('temp')
+    top_p = data.get('topP')
+    top_k = data.get('topK')
+    n_predict = data.get('nPredict')
+    frequency_penalty =  data.get('frequency_penalty')
+    presence_penalty = data.get('presence_penalty')
+    logger.info('completion() data:'+str(data))
     if prompt:
-        result_completion = LLMFactory.create_llm(llm_name).completion(prompt)
+        result_completion = LLMFactory.create_llm(llm_name).completion(prompt, temperature, top_p, top_k, n_predict, presence_penalty, frequency_penalty )
+
+        #Q: replace \begin{code}  with <code> in result_completion and replace \end{code} with </code>
+        result_completion = result_completion.replace("\\begin{code}", "```")
+        result_completion = result_completion.replace("\\end{code}", "```")
+
+        #Q: replace \item with \n\n- in result_completion
+        result_completion = result_completion.replace("\\item", "\n\n-")
+
+
+        print(result_completion)
         return jsonify({'completion': result_completion})
 
     return ""
-
 
